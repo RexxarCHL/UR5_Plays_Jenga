3bbdb5324143e00a1a2917fed239d51a538dbfd8
clin110 <clin110@jhu.edu>
2018-04-26 17:45:07 -0400

    Modify py to just publish the location. Push the error handling back the chain

--- jenga_calibrate/scripts/ar_marker_to_tower_tf_broadcaster.py
+++ jenga_calibrate/scripts/ar_marker_to_tower_tf_broadcaster.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 import rospy
 import tf
-import numpy as np
 
 if __name__ == '__main__':
 	rospy.init_node('base_link_to_camera_tf_broadcaster')
@@ -9,30 +8,18 @@ if __name__ == '__main__':
 	listener = tf.TransformListener()
 	broadcaster = tf.TransformBroadcaster()
 
+	frame_exists = \
+		listener.waitForTransform('base_link', 'ar_marker_1', 
+								  rospy.Time(0),rospy.Duration(5.0))
+
+
 	rate = rospy.Rate(10.0)
 	while not rospy.is_shutdown():
-		try: # Get the transformation to ar_marker_1 from base_link
-			(trans, rot) = listener.lookupTransform('base_link', 'ar_marker_1', rospy.Time(0))
-		except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
-			continue
-
-		tf_base_marker = listener.fromTranslationRotation(trans, rot)
-
-		rpy = tf.transformations.euler_from_quaternion(rot)
-		rot = tf.transformations.quaternion_from_euler(-rpy[0], -rpy[1], 0)
-		tf_marker_to_tower = listener.fromTranslationRotation(
-				(-0.065, -0.065, 0),
-				tuple(rot) )
-
-		tf_base_tower = tf_base_marker * tf_marker_to_tower
-		_, _, rpy, trans, _ = tf.transformations.decompose_matrix(tf_base_tower)
-		rot = tf.transformations.quaternion_from_euler(rpy[0], rpy[1], rpy[2])
-
 		broadcaster.sendTransform( 
-				tuple(trans),
-				tuple(rot),
+				(-0.065, -0.065, 0),
+				(0, 0, 0, 1),
 				rospy.Time.now(),
 				'ar_tower_location',
-				'base_link')
+				'ar_marker_1')
 		rate.sleep()
 		
