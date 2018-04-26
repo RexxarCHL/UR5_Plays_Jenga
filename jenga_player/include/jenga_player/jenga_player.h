a2f861a083e2b37b70353fe8009d1723b8b70fd4
clin110 <clin110@jhu.edu>
2018-04-25 22:52:24 -0400

    Implemented reflex agent

--- jenga_player/include/jenga_player/jenga_player.h
+++ jenga_player/include/jenga_player/jenga_player.h
@@ -1,6 +1,6 @@
 /**
  * Jenga Player
- * The AI brain that plays Jenga. A simple reflex agent that tries to play every middle block from level 6 and up.
+ * The AI brain that plays Jenga.
  * Author: Chia-Hung Lin (clin110[AT]jhu[DOT]edu)
  * Date Created: 04/23/2018
  */
@@ -10,31 +10,51 @@
 #include <ros/ros.h>
 
 #include <jenga_msgs/JengaTarget.h>
-#include <jenga_msgs/JengaTargets.h>
+//#include <jenga_msgs/JengaTargets.h>
 #include <jenga_msgs/JengaTargetResult.h>
 
 class JengaAgent // base class for all agents
 {
 public:
-  virtual ~JengaAgent() {};
-  virtual void publishNextTarget() = 0;
+  virtual ~JengaAgent() {}; // Default destructor
+  virtual void publishNextTarget() = 0; // Virtual function for the derived class to implement
 protected:
-  const int START_TOWER_LEVEL_ {18};
-  const int START_TARGET_LEVEL_ {6}; 
+  const int START_TOWER_LEVEL_ {18}; // A full tower starts at level 18
+  const int START_TARGET_LEVEL_ {6}; // Start probing from level 6 due to clearance issue
+
   ros::NodeHandle nh_;
-  ros::Publisher jenga_targets_publisher_;
+  ros::Publisher jenga_target_publisher_;
   ros::Subscriber target_result_subscriber_;
+  
+  // Game state
   int current_tower_level_;
   int target_level_;
 }; // JengaAgent
 
+/**
+ * ReflexAgent: 
+ *   A simple reflex agent that tries to play every legal middle block from the tower.
+ */
 class ReflexAgent : public JengaAgent
 {
 public:
   ReflexAgent(ros::NodeHandle* nh);
-  void publishNextTarget();
+  void publishNextTarget(); // Inherited
 private:
+  int top_block_count_; // Track how many blocks are currently on the top.
+  /**
+   * Process the result and publish the next target block.
+   */
   void targetResultCallback(const jenga_msgs::JengaTargetResult::ConstPtr& result);
+  /**
+   * Increment top block count; increment current tower level if 3 blocks are on the top.
+   */
+  void updateGameState();
+  /**
+   * Get which side to drive to from the input level.
+   */
+  int getSide(int level);
+
 }; // ReflexAgent
 
 #endif // JENGA_PLAYER_H
--- jenga_player/src/jenga_player.cpp
+++ jenga_player/src/jenga_player.cpp
@@ -1,21 +1,82 @@
 #include "jenga_player/jenga_player.h"
 
+// Constructor for the reflex agent
 ReflexAgent::ReflexAgent(ros::NodeHandle* nh)
 {
+  ROS_INFO("1");
   nh_ = *nh;
   target_result_subscriber_ = nh_.subscribe("/jenga/target_result", 3, &ReflexAgent::targetResultCallback, this);
-  jenga_targets_publisher_ = nh_.advertise<jenga_msgs::JengaTargets>("/jenga/targets", 1, true);
+  jenga_target_publisher_ = nh_.advertise<jenga_msgs::JengaTarget>("/jenga/target", 1);
+
+  ROS_INFO("2");
+
+  current_tower_level_ = START_TOWER_LEVEL_;
+  target_level_ = START_TARGET_LEVEL_;
+  top_block_count_ = 0;
+
+  publishNextTarget(); // Publish the first block immediately after initialization
 }
 
+// If the result is successful, update game state. Publish the next block no matter the result.
 void ReflexAgent::targetResultCallback(const jenga_msgs::JengaTargetResult::ConstPtr& result)
 {
   // do something
+  if (result->result) // Probing successful
+  {
+    updateGameState();
+  }
+
+  if (target_level_ < current_tower_level_ - 1)
+    publishNextTarget(); // Next move is legal. OK to publish
+  else
+  {
+    // No more possible moves
+    ROS_FATAL("No more possible moves. Spinning...");
+    ros::spin();
+  }
 }
 
+// Increment top block count; increment current tower level if 3 blocks are on the top.
+void ReflexAgent::updateGameState()
+{
+  // Put another block on top of the tower
+  top_block_count_ += 1;
+
+  // One full level of tower completed
+  if (top_block_count_ > 3)
+  {
+    current_tower_level_ += 1;
+    top_block_count_ = 0;
+  }
+
+  ROS_INFO("Current tower level: %d, target level: %d, block count: %d", 
+      current_tower_level_, target_level_, top_block_count_);
+}
+
+// Get which side to drive to from the input level.
+int ReflexAgent::getSide(int level)
+{
+  return level % 2; // for side 0 and 1
+  // or level % 2 + 2 for side 2 and 3
+}
+
+// Try to play every legal middle block from the tower.
 void ReflexAgent::publishNextTarget()
 {
-  // TODO
-  ROS_INFO("%d %d", START_TOWER_LEVEL_, START_TARGET_LEVEL_);
+  jenga_msgs::JengaTarget target;
+
+  // Initialize the target message
+  target.side = getSide(target_level_);
+  target.level = target_level_;
+  target.block = 0; // Aim for the middle block
+
+  // Increment target_level_ for next target
+  target_level_++;
+
+
+  ROS_INFO("Publishing target: side%d, level%d", target.side, target.level);
+  jenga_target_publisher_.publish(target);
+  ROS_INFO("OK");
 }
 
 int main(int argc, char** argv)
@@ -23,7 +84,8 @@ int main(int argc, char** argv)
   ros::init(argc, argv, "jenga_player");
 
   ros::NodeHandle nh;
-  ReflexAgent agent();
+  ReflexAgent agent(&nh);
+
 
   ros::spin();
 
