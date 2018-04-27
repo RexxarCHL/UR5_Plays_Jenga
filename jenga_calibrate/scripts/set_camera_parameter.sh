55a89b115391193c644dbc2e9ab85e955af789bd
clin110 <clin110@jhu.edu>
2018-04-26 20:23:01 -0400

    Changed a camera; modified some parameters

--- jenga_calibrate/scripts/set_camera_parameter.sh
+++ jenga_calibrate/scripts/set_camera_parameter.sh
@@ -4,7 +4,8 @@ video0=/dev/video0
 if [ -e $video0 ]
 	then
 	v4l2-ctl -d $video0 -c focus_auto=0
-	v4l2-ctl -d $video0 -c focus_absolute=3
+	v4l2-ctl -d $video0 -c focus_absolute=0
+	v4l2-ctl -d $video0 -c zoom_absolute=500
 	echo Successfully set camera focus for $video0.
 else
 	echo $video0 not found.
