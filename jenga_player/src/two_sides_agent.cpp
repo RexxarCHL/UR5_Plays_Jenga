acd93a88db5bc64134dd09cc4033b7398a969154
clin110 <clin110@jhu.edu>
2018-05-03 15:36:58 -0400

    Further implementation of two sides agent

--- jenga_player/src/two_sides_agent.cpp
+++ jenga_player/src/two_sides_agent.cpp
@@ -8,8 +8,16 @@ TwoSidesAgent::TwoSidesAgent(ros::NodeHandle* nh)
   jenga_target_publisher_ = nh_.advertise<jenga_msgs::JengaTarget>("/jenga/target", 1);
 
   current_tower_level_ = START_TOWER_LEVEL_;
-  target_level_ = START_TARGET_LEVEL_;
+
   top_block_count_ = 0;
+  playing_side_ = getSide();
+  side_levels_[0] = START_TARGET_LEVEL_;
+  side_levels_[1] = START_TARGET_LEVEL_;
+  side_blocks_[0] = 0;
+  side_blocks_[1] = 0;
+  playing_level_ = side_levels_[playing_side_ % 2] - 2;
+  playing_block_ = -1;
+  playing_result_ = false;
 
   // Wait for other nodes to subscribe to this message
   ros::Rate poll_rate(10);
@@ -27,7 +35,7 @@ TwoSidesAgent::TwoSidesAgent(ros::NodeHandle* nh)
 // If the result is successful, update game state. Publish the next block no matter the result.
 void TwoSidesAgent::targetResultCallback(const jenga_msgs::JengaTargetResult::ConstPtr& result)
 {
-  previous_result_ = result->result;
+  playing_result_ = result->result;
 
   // do something
   if (result->result) // Probing successful
@@ -35,7 +43,7 @@ void TwoSidesAgent::targetResultCallback(const jenga_msgs::JengaTargetResult::Co
     updateGameState();
   }
 
-  if (target_level_ < current_tower_level_ - 1)
+  if (side_levels_[playing_side_ % 2] < current_tower_level_ - 1)
     publishNextTarget(); // Next move is legal. OK to publish
   else
   {
@@ -59,7 +67,7 @@ void TwoSidesAgent::updateGameState()
   }
 
   ROS_INFO("Current tower level: %d, target level: %d, block count: %d", 
-      current_tower_level_, target_level_, top_block_count_);
+      current_tower_level_, side_levels_[playing_side_], top_block_count_);
 }
 
 /**
@@ -67,7 +75,8 @@ void TwoSidesAgent::updateGameState()
  */
 int TwoSidesAgent::getSide()
 {
-  return level % 2; // for side 0 and 1
+  return 0;
+  //return level % 2; // for side 0 and 1
   // or level % 2 + 2 for side 2 and 3
 }
 
@@ -79,52 +88,50 @@ void TwoSidesAgent::publishNextTarget()
   target.side = getSide(); // Check if the tower orientation is changed drastically
 
   // Initialize the target message
-  if (previous_result_) // If previous action is successful
+  if (playing_result_) // If previous action is successful
   {
     // Go to next level and try to push the middle block
-    target.level = previous_side_ + 2; // +2 because we want to play blocks on the same side
+    target.level = playing_level_ + 2; // +2 because we want to play blocks on the same side
     target.block = 0; // Aim for the middle block
   }
   else // If previous action is NOT successful
   {
     // It is very likely that the blocks on the sides are loose
-    if (previous_block_ != -1) // Previous block is not the left block 
+    if (playing_block_ != -1) // Previous block is not the left block 
     {
       // Stay on this level
-      target.level = previous_side_;
+      target.level = playing_level_;
       // If previous block is the middle block, try pushing the one to its right;
       // If previous block is the right block, try pushing the left block
-      target.block = (previous_block_ == 0)? 1 : -1;
+      target.block = (playing_block_ == 0)? 1 : -1;
     }
     else // Previous block is the left block
     {
       // All block on this level is probed and not removeable. Move to next level
-      target.level = previous_side_ + 2; // +2 because we want to play blocks on the same side
+      target.level = playing_side_ + 2; // +2 because we want to play blocks on the same side
       target.block = 0;
     }
   }
 
-  if (target.side != previous_side_ && target.side % 2 != previous_side_ % 2) 
+  if (target.side != playing_side_ && target.side % 2 != playing_side_ % 2) 
   { 
     // Side is changed!
     ROS_WARN("SIDE CHANGED!");
     // Store the game status to play the next block on this side;
-    int side_index = previous_side_ % 2;
-    side_levels[side_index] = target.level;
-    side_blocks[side_index] = target.block;
+    side_levels_[playing_side_ % 2] = target.level;
+    side_blocks_[playing_side_ % 2] = target.block;
 
     // Overwrite the previous assignments to adapt to the new side
-    int new_side_index = target.side;
-    target.level = side_levles[new_side_index];
-    target.block = side_levels[new_side_index];
+    playing_side_ = target.side;
+    target.level = side_levels_[playing_side_ % 2];
+    target.block = side_blocks_[playing_side_ % 2];
   }
-
 
   ROS_INFO("Publishing target: side%d, level%d, block %d", target.side, target.level, target.block);
   jenga_target_publisher_.publish(target);
 
-  // Increment target_level_ for next target
-  target_level_++;
+  playing_level_ = target.level;
+  playing_block_ = target.block;
 }
 
 int main(int argc, char** argv)
